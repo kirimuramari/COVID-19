38b39bb5d5f7a187ce3e0c26c8e5c5c2dda6aff0
kirimuramari <lifemastert@gmail.com>
2025-04-05 22:02:58 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
+
+//スマホ表示
+const deviceMessageSpan = document.getElementsByClassName('device-message');
+function updateDeviceMessage() {
+    const isMobile = /iPhone|iPad|iPod|Android/.test(navigator.userAgent);
+    if (isMobile) {
+        deviceMessageSpan.textContent = 'デバイス';
+    } else {
+        deviceMessageSpan.textContent = 'コンピュータ';
+    }
+}
+// ページ読み込み時にメッセージを更新
+updateDeviceMessage();
 fetch('https://ip-api.com/json')
     .then(response => response.json())
     .then(data => {
@@ -26,18 +39,8 @@ function updateCountdown() {
         countdownDisplay.textContent = '00:00';
         alert('カウントダウン終了！');
     }
-}
-//スマホ表示
-const deviceMessageSpan = document.getElementsByClassName('device-message');
-function updateDeviceMessage() {
-    const isMobile = /iPhone|iPad|iPod|Android/.test(navigator.userAgent);
-    if (isMobile) {
-        deviceMessageSpan.textContent = 'デバイス';
-    } else {
-        deviceMessageSpan.textContent = 'コンピュータ';
     }
-}
+
 // カウントダウンを開始
 updateCountdown();
-// ページ読み込み時にメッセージを更新
-updateDeviceMessage();
+
